5cee5da319651b72ee49efbb6fccc828ee0b9030
hrs13 <hrs13@ic.ac.uk>
2018-06-19 21:26:31 +0100

    travis pytest tweak

--- tests/travis_prelim.py
+++
@@ -1,8 +0,0 @@
-"""
-All the classification data come in a single large download. If many tests are run in parallel this can cause 
-issues of synchrony. We run this once on travis so the data is there.   
-"""
-
-from bayesian_benchmarks.data import ALL_CLASSIFICATION_DATATSETS
-
-ALL_CLASSIFICATION_DATATSETS['iris']()
