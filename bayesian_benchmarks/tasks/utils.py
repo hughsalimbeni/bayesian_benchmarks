088e08e67eda28a39ca87f1dcd0f6167b325da72
Felix Leibfried <felix@felix-dsktp.piointernal.prowler.io>
2020-02-10 12:04:45 +0000

    changed comment

--- bayesian_benchmarks/tasks/utils.py
+++ bayesian_benchmarks/tasks/utils.py
@@ -10,8 +10,8 @@ from scipy.special import logsumexp
 
 def meansumexp(logps: List[np.ndarray]) -> np.ndarray:
     """
-    Mean sum exp of a log p array.
-    :param logps: list of log probs [samples x data points]
+    Mean sum exp of a log p list.
+    :param logps: list of log probs [samples x data points] or [samples x data points x output dim]
     :return: avg probability value [1]
     """
     return np.mean(logsumexp(logps, axis=0) - np.log(len(logps)))
